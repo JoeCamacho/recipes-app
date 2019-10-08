9ce7cf1b5ff05bbe8fb982ae4dea092694422811
Joel <camacho-joel@hotmail.com>
2019-10-08 18:13:53 +0200

    creating ShoopingListComponent

--- recipes-app/src/app/app.module.ts
+++ recipes-app/src/app/app.module.ts
@@ -7,9 +7,10 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoopingListComponent } from './shooping-list/shooping-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent, ShoopingListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
