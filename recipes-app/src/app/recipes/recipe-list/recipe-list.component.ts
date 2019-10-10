153fcf55c59320afa05ba74b5d7f683aa41682d9
Joel <camacho-joel@hotmail.com>
2019-10-10 14:55:49 +0200

    using the recipe model

--- recipes-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipes-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "this is a test",
+      "https://images.ctfassets.net/uexfe9h31g3m/1cnOikouN2MOQ6aUoAIagw/5cbe300e1a7b84ae9b195eeddfe4cd73/quorn-vegan-jamaican-fillets.jpg?w=1024&h=576&fm=jpg&fit=thumb&q=70&fl=progressive"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
